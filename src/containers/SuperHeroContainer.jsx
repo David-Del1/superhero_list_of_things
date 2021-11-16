6329ef79917c99090fc89d930bb27c847283c8fe
David Delgadillo <daviddel.731@gmail.com>
2021-11-16 14:17:11 -0800

    Fixed eslint issue

--- src/containers/SuperHeroContainer.jsx
+++ src/containers/SuperHeroContainer.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import CharacterCard from '../components/characterCard/CharacterCard';
 import CharacterList from '../components/characterList/CharacterList';
 import CharacterSearch from '../components/search/CharacterSearch';
 
