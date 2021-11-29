92be9d524cb5baec2f963f795469e4edf4716d14
David Delgadillo <daviddel.731@gmail.com>
2021-11-29 13:03:21 -0800

    enhanced pagination experience

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -5,24 +5,40 @@ import './Pagination.scss';
 const Pagination = ({ charactersPerPage, totalCharacters, paginate, currentPage }) => {
   const pageNumbers = [];
 
-  for(let i = 1; i <=Math.ceil(totalCharacters / charactersPerPage); i++) {
+  for(let i = 1; i <= Math.ceil(totalCharacters / charactersPerPage); i++) {
     pageNumbers.push(i);
   }
   return (
     <nav>
       <ul className="Pagination">
+      {currentPage !== 1 ? <button 
+        onClick={() => paginate(currentPage - 1)}
+        className="pagination-arrow"
+      >
+        {'←'}
+      </button>
+      : null}
         {
           pageNumbers.map(num => (
+            
             <li key={num} className={num === currentPage ? 'active' : 'default'}>
               <a onClick={() => paginate(num)} href="!#">
                 {num}
               </a>
               
             </li>
-          ))
+          )).slice(currentPage - 1, currentPage + 3)
         }
-      </ul>
+        {(currentPage === 26 || currentPage === 27 || currentPage === 28 || currentPage === 29) ? null : '...' }
 
+        {currentPage !== pageNumbers.length ? <button 
+          onClick={() => paginate(currentPage + 1)}
+          className="pagination-arrow"
+        >
+        {'→'}
+        </button>
+        : null}
+      </ul>
     </nav>
   );
 }
